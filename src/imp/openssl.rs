93b81366d1c616df02bfb381e7d5fd39e8fdba55
Armin Widegreen <armin.widegreen@rco.se>
2021-09-30 13:07:17 +0200

    Adds client authentication

--- src/imp/openssl.rs
+++ src/imp/openssl.rs
@@ -17,7 +17,7 @@ use std::io;
 use std::sync::Once;
 
 use self::openssl::pkey::Private;
-use {Protocol, TlsAcceptorBuilder, TlsConnectorBuilder};
+use {Protocol, TlsAcceptorBuilder, TlsClientCertificateVerification, TlsConnectorBuilder};
 
 #[cfg(have_min_max_version)]
 fn supported_protocols(
@@ -342,6 +342,22 @@ impl TlsAcceptor {
         let mut acceptor = SslAcceptor::mozilla_intermediate(SslMethod::tls())?;
         acceptor.set_private_key(&builder.identity.0.pkey)?;
         acceptor.set_certificate(&builder.identity.0.cert)?;
+
+        if let Some(client_ca_cert) = &builder.client_cert_verification_ca_cert {
+            acceptor.add_client_ca((client_ca_cert.0).0.as_ref())?;
+            // below call is required if the ca is not already trusted
+            acceptor
+                .cert_store_mut()
+                .add_cert((client_ca_cert.0).0.to_owned())?;
+        }
+        let verify_mode = match &builder.client_cert_verification {
+            TlsClientCertificateVerification::DoNotRequestCertificate => SslVerifyMode::NONE,
+            TlsClientCertificateVerification::RequestCertificate => SslVerifyMode::PEER,
+            TlsClientCertificateVerification::RequireCertificate => {
+                SslVerifyMode::PEER | SslVerifyMode::FAIL_IF_NO_PEER_CERT
+            }
+        };
+        acceptor.set_verify(verify_mode);
         for cert in builder.identity.0.chain.iter().rev() {
             acceptor.add_extra_chain_cert(cert.to_owned())?;
         }
